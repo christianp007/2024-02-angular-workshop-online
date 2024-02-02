57bd44d775823e1b775cce0d21570c20f6148c41
Ferdinand Malcher <ferdinand@malcher.media>
2024-02-02 14:12:09 +0100

    Routing: Redirect, Links

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { booksRoutes } from './books/books.routes';
 
 export const routes: Routes = [
+  // bei Weiterleitung vom leeren Pfad ist fast immer "pathMatch: full" nötig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
   ...booksRoutes
 ];
