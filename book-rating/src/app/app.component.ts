6fddb78ab74e8499b88f5a55f35495e5091bd41e
Ferdinand Malcher <ferdinand@malcher.media>
2024-02-02 13:34:00 +0100

    Routen, RouterOutlet

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from "./books/dashboard/dashboard.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, DashboardComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'Book Rating';
