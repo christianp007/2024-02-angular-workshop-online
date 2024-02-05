8ea92b49ab14a6edb7d6ca91f9a9c76f9d0557b6
Ferdinand Malcher <ferdinand@malcher.media>
2024-02-05 13:41:23 +0100

    Reactive Forms

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,12 +1,45 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
-
+  bookForm = new FormGroup({
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    description: new FormControl('', {
+      nonNullable: true
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.min(0),
+        Validators.max(10000)
+      ]
+    }),
+    rating: new FormControl(1, {
+      nonNullable: true,
+      validators: [
+        Validators.min(1),
+        Validators.max(5),
+      ]
+    }),
+  });
 }
