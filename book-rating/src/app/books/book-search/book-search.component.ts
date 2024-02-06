46e9548c70688f19bbadb4db1c25b229d1145ef8
Ferdinand Malcher <ferdinand@malcher.media>
2024-02-06 15:06:08 +0100

    Vorbereitung für Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-search',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book-search.component.html',
   styleUrl: './book-search.component.scss'
 })
 export class BookSearchComponent {
+  searchControl = new FormControl('', { nonNullable: true });
+  private bs = inject(BookStoreService);
 
+  constructor() {
+    this.searchControl.valueChanges.subscribe(e => {
+      console.log(e);
+    });
+  }
 }
