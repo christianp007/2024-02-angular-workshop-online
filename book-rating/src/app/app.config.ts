6d41f4298f1a890d34e3a5798c2bad9c8f865ebd
Ferdinand Malcher <ferdinand@malcher.media>
2024-02-07 12:27:51 +0100

    NgRx einrichten

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import localeDe from '@angular/common/locales/de';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 registerLocaleData(localeDe);
 
@@ -12,6 +15,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    { provide: LOCALE_ID, useValue: 'de' }
-  ]
+    { provide: LOCALE_ID, useValue: 'de' },
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
+]
 };
